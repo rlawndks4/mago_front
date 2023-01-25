6a691176ea131e04c06a2b5925dc4e1b55abff69
rlawndks4 <rlawndks0423@naver.com>
2023-01-25 17:26:15 +0900

    ASfd

--- src/pages/User/Home.js
+++ src/pages/User/Home.js
@@ -161,7 +161,7 @@ const Home = () => {
     };
     useEffect(() => {
         async function fetchPost() {
-           // setLoading(true)
+            setLoading(true)
             const { data: response } = await axios.get('/api/gethomecontent')
             let banner_list = [];
             let banner_link_obj = {};
