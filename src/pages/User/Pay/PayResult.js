65ab0ee9a1c41e5510e4f5a2aaef12bbd085f1fb
rlawndks4 <rlawndks0423@naver.com>
2023-02-01 16:54:36 +0900

    asdf

--- src/pages/User/Pay/PayResult.js
+++ src/pages/User/Pay/PayResult.js
@@ -37,13 +37,18 @@ const PayResult = () => {
                         {commarNumber(makeDiscountPrice(posts?.price, posts?.discount_percent))}원
                     </div>
                 </RowContent>
-                <ShadowContainer style={{ display: 'flex', flexDirection: 'column', height: '300px' }}>
+                <ShadowContainer style={{ display: 'flex', flexDirection: 'column', height: '450px' }}>
                     {params?.status == 1 ?
                         <>
                             <div style={{ margin: 'auto auto 8px auto' }}>결제가 완료되었습니다.</div>
                             <div style={{ margin: '8px auto 8px auto' }}>잠시후 카카오톡(위드싸인)으로 계약서가 발송됩니다.</div>
                             <div style={{ margin: '8px auto 8px auto' }}>결제내역은 마이페이지에서 확인하실 수 있습니다.</div>
-                            <div style={{ margin: '8px auto 8px auto',color:theme.color.red }}>결제하신 내용은 시스템상 자동확인 후 반영되오니 5~10분 이내로 적용됩니다.</div>
+                            <div style={{ margin: '8px auto 8px auto' }}>주식과 관련한 카드결제의 경우 할부결제가 진행되지 않습니다.</div>
+                            <div style={{ margin: '8px auto 8px auto' }}>때문에, 결제사를 통해 우회해서 결제가 진행되는 점 양해바랍니다.</div>
+                            <div style={{ margin: '8px auto 8px auto',color:theme.color.red  }}>혹시라도, 카드사에거 결제 관련 문의 전화가 올 경우,</div>
+                            <div style={{ margin: '8px auto 8px auto',color:theme.color.red  }}>필리핀 세부 여행 패키지 상품 으로 결제를 진행했다고, 답해주시면 감사하겠습니다.</div>
+                            <div style={{ margin: '8px auto 8px auto',color:theme.color.red  }}>(만일 주식 관련 내용으로 답변이 될 경우 할부 결제가 취소됩니다.)</div>
+                            <div style={{ margin: '8px auto 8px auto'}}>결제하신 내용은 시스템상 자동확인 후 반영되오니 5~10분 이내로 적용됩니다.</div>
                             <BsCheck2All style={{ margin: '8px auto auto auto', fontSize: theme.size.font1 }} />
                         </>
                         :
