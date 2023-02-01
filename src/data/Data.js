a79b11eee15b0839a09757513f387c0b86177926
rlawndks4 <rlawndks0423@naver.com>
2023-02-01 20:20:10 +0900

    asdf

--- src/data/Data.js
+++ src/data/Data.js
@@ -26,7 +26,7 @@ import axios from 'axios';
 const test = true;
 
 //export const frontUrl = "http://localhost:3000";
-export const backUrl = "https://1st-academy.kr:8443";
+export const backUrl = "https://purplevery13.cafe24.com:8443";
 export const frontUrl = "https://1st-academy.kr";
 //export const backUrl = "https://1st-academy.kr:8443";
 
