557c32892a0e81bf2e6aa5e5bfc6c644a6b307f5
rlawndks4 <rlawndks0423@naver.com>
2023-02-06 10:50:30 +0900

    asdf

--- src/pages/User/EnrolmentList.js
+++ src/pages/User/EnrolmentList.js
@@ -56,9 +56,8 @@ const EnrolmentList = () => {
 
     useEffect(() => {
         async function fetchPost() {
-            //setLoading(true)
+            setLoading(true)
             const { data: response } = await axios.get('/api/getenrolmentlist');
-            console.log(response)
             let banner_list = [];
             for (var i = 1; i <= 5; i++) {
                 if (response?.data?.banner[`enrolment_banner_img_${i}`]) {
